de062b0e080eb672e5c69779fd4a4c8fa0452d1b
amitgabso <amitgabso23@gmail.com>
2019-04-16 14:47:08 +0300

    fixed securty issues

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,19 +1,27 @@
 class CarsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @cars = Car.all
   end
 
   def new
-    @car = current_user.cars.build
+    if current_user.is_admin
+      @car = current_user.cars.build
+    else
+      flash[:notice] = 'Unauthorized: Not an admin'
+      redirect_to cars_path
+    end
+
   end
 
   def create
     @car = current_user.cars.build(car_params)
-    #@car.user = current_user
 
     @car.save
     redirect_to @car
+
   end
 
   def show
